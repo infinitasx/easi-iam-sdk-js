ce4052345c9731077884b12caa813f68283ce1b9
唐海 <280503392@qq.com>
2021-07-27 15:57:03 +0800

    feat: Vite配置完善

---
+++ dist/constant/index.d.ts
@@ -0,0 +1,3 @@
+export declare const DEVELOPMENT_URL = "https://accounts-admin.melbdelivery.com";
+export declare const TESTING_URL = "https://accounts-admin.melbdelivery.com";
+export declare const PRODUCTION_URL = "https://accounts-admin.easi.com.au";
---
+++ dist/utils/easiOidcClient.d.ts
@@ -0,0 +1,2 @@
+import { Params, ResultType } from '../type/settings';
+export default function (params: Params): ResultType;
