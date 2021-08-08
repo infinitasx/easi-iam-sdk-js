659984876f2385841384f621ec9872627da44996
tanxin <txloveu@163.com>
2021-08-08 18:56:32 +0800

    chore: 拆分UI

---
+++ UI/vue3&antd/index.js
@@ -0,0 +1,17 @@
+import {Modal, message} from "ant-design-vue";
+import CallbackPage from './views/Callback.js'
+
+export default {
+  showErrorMsg: (msg) => message.error(msg),
+  showTokenExpiredModal: ({title, content, okText}, callback) => {
+    Modal({
+      title,
+      content,
+      okText,
+      onOk() {
+        callback();
+      }
+    })
+  },
+  codeExchangeTokenPage: CallbackPage,
+}
