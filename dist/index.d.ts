edcec6e57176156d8add1e864a2a799fc5f09f6a
tanxin <txloveu@163.com>
2021-08-23 16:01:26 +0800

    chore: 修改版本导入错误

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,2 @@
 export { default as IamClient } from './utils/easiOidcClient';
-export declare const version = "0.0.20";
+export declare const version = "0.0.21";
--- packages/index.ts
+++ packages/index.ts
@@ -1,2 +1,2 @@
 export { default as IamClient } from './utils/easiOidcClient';
-export const version = '0.0.20';
+export const version = '0.0.21';
