4575109672cbf7ba32b0192ae93b4cfb9c8e9acb
tanxin <txloveu@163.com>
2021-11-25 16:34:30 +0800

    chore: 创建消息中间页面排除iam的

--- packages/utils/message.ts
+++ packages/utils/message.ts
@@ -1,4 +1,5 @@
 import { iframeIdName } from '../constant';
+import { PRODUCTION_URL, TESTING_URL } from '../constant';
 
 /**
  * iam 和 相同浏览器的各个子系统页面的消息传递
@@ -6,11 +7,16 @@ import { iframeIdName } from '../constant';
 
 // 创建中间iframe页面
 export function _createFrame(url: string) {
+  // 排除 iam 的正式、测试环境
+  const href = location.href;
+  if (href.indexOf(PRODUCTION_URL) > -1 || href.indexOf(TESTING_URL) > -1) {
+    return;
+  }
   const iframe = document.createElement('iframe');
   iframe.src = url;
   iframe.width = '0';
   iframe.height = '0';
-  iframe.style.border = 'none;';
+  iframe.style.border = 'none';
   iframe.id = iframeIdName;
   document.body.appendChild(iframe);
 }
@@ -26,7 +32,5 @@ export function sendMessageToIAM(type: string, message: string) {
       },
       '*',
     );
-  } else {
-    throw new Error('not found element：id=' + iframeIdName);
   }
 }
